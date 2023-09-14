d8e2547953f45cebd17eb276fd4e52ac9b541a1f
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-09-14 10:56:08 -0300

    [change] descriptive docker run

--- cron/daily-watcher.sh
+++ cron/daily-watcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker compose --project-directory /docker/link-watcher/ up watcher
+/usr/bin/docker run --rm --name link-watcher -v /docker/link-watcher/volumes/watcher:/tmp/watcher --env-file /docker/link-watcher/.env registry.pop-pr.rnp.br/library/link-watcher:$(grep -oP '^VERSION=\K.*' /docker/link-watcher/.env)
--- cron/weekly-alert.sh
+++ cron/weekly-alert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker compose up alert-weekly
+/usr/bin/docker run --rm --name link-watcher -v /docker/link-watcher/volumes/watcher:/tmp/watcher --env-file /docker/link-watcher/.env registry.pop-pr.rnp.br/library/link-watcher:$(grep -oP '^VERSION=\K.*' /docker/link-watcher/.env) python3 alert.py -d /tmp/watcher/ -f /tmp/watcher/hosts.json
