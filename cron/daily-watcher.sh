a42dec8fa5afb45f8c5b8e105008c085951c3bcc
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-09-27 10:47:31 -0300

    [change] project path

--- cron/daily-watcher.sh
+++ cron/daily-watcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker run --rm --name link-watcher -v /docker/link-watcher/volumes/watcher:/tmp/watcher --env-file /docker/link-watcher/.env registry.pop-pr.rnp.br/library/link-watcher:$(grep -oP '^VERSION=\K.*' /docker/link-watcher/.env)
+/usr/bin/docker run --rm --name link-watcher -v /link-watcher/volumes/watcher:/tmp/watcher --env-file /link-watcher/.env link-watcher
