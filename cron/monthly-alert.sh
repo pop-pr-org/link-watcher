9d5d2a217d6cae33a70e0bf25ce5d48d77007fbf
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-09-27 10:47:20 -0300

    [fix] now overriding entrypoint to use the alert script

--- cron/monthly-alert.sh
+++ cron/monthly-alert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/docker run --rm --name link-watcher -v /docker/link-watcher/volumes/watcher:/tmp/watcher --env-file /docker/link-watcher/.env registry.pop-pr.rnp.br/library/link-watcher:$(grep -oP '^VERSION=\K.*' /docker/link-watcher/.env) python3 alert.py -d /tmp/watcher/ -f /tmp/watcher/hosts.json -n 30
+/usr/bin/docker run --rm --name link-watcher -v /link-watcher/volumes/watcher:/tmp/watcher --env-file /link-watcher/.env --entrypoint "python3" link-watcher alert.py -d /tmp/watcher/ -f /tmp/watcher/hosts.json
