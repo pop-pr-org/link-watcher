7b0eaae9f90232f15c4fc6f07588c6c8627aedf6
Vinicius Fontoura de Abreu <vinicius.fontoura@pop-pr.rnp.br>
2023-10-04 10:52:32 -0300

    [removed] unecessary code

--- tsdb/TsdbExtractor.py
+++ tsdb/TsdbExtractor.py
@@ -3,12 +3,13 @@
 
 from pprint import pprint
 from datetime import datetime, timedelta
-from tsdb import Tsdb
-from influxdb import InfluxDBClient
+from pytz import timezone as tz
 from logging import getLogger
 from os import environ
 
-from config import LOGGER_NAME
+from tsdb import Tsdb
+from influxdb import InfluxDBClient
+from config import LOGGER_NAME, OUTPUT_TIMEZONE, TSDB_TIME_FORMAT
 
 logger = getLogger(LOGGER_NAME)
 
@@ -36,51 +37,34 @@ class TsdbExtractor(Tsdb):
         link_configs: dict, iface: str, db_client: InfluxDBClient
     ) -> list:
         """
-        query TSDB for a given host iface traffic with a set time range
+        query TSDB for a given host interface (rx|tx) traffic with a set time range
+
+        Params:
+        - link_configs: dict with the following format:
+            {
+                "LINKS": [
+                    {
+                        "LINK_SPEED": speed in bits,
+                        "LINK_NAME": name of the link in your IRM
+                    },
+                ]
+            }
+        - iface: interface name (rx|tx)
+        - db_client: tsdb client object
+
+        Returns:
 
         Should return a list of dicts with AT LEAST these key:value pairs:
 
         [
             {
                 "time": timestamp,
-
                 "value": value in bits
             },
         ]
 
+        If no data is found, returns an empty list
+
         """
-
-        starting_time = environ["QUERY_BEGIN"]
-        starting_time = datetime.strptime(starting_time, "%Y-%m-%d %H:%M:%S")
-        # set the time format
-        # Influx is using utc so we need to add 3 hours to get the correct time
-        # during the report building process we will subtract 3 hours
-        starting_time = starting_time + timedelta(hours=3)
-
-        ending_time = environ["QUERY_END"]
-        ending_time = datetime.strptime(ending_time, "%Y-%m-%d %H:%M:%S")
-        # Influx is using utc so we need to add 3 hours to get the correct time
-        # during the report building process we will subtract 3 hours
-        ending_time = ending_time + timedelta(hours=3)
-
-        # querying influxdb
-        try:
-            query = 'SELECT * from "check_iface_traffic" WHERE "time" >= \'{}\' AND "time" <= \'{}\' AND  \
-                "hostname" = \'{}\' AND "metric" = \'iface-traffic{}\''.format(
-                starting_time, ending_time, link_configs["LINK_NAME"], iface
-            )
-            tag = db_client.query(query)
-        except Exception as e:
-            logger.error("error querying influxdb: %s", e)
-            return []  # returning empty list
-
-        # extracting points
-        tag = tag.get_points()
-
-        # data needs to be a list of dicts
-        # with at least 'time' and 'value' keys
-        # removing None values
-        data = [t for t in tag if t["value"] is not None]
-        # pprint(data)
-
+        data = []
         return data
