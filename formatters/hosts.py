3414c12f85220f12e0a4033a0c5fc94159944b92
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-02-20 13:59:31 -0300

    [refactor] links file structure changed

--- formatters/hosts.py
+++ formatters/hosts.py
@@ -7,7 +7,7 @@ from config import LOGGER_NAME, DEFAULT_LINK_HISTERESYS, DEFAULT_MAX_TRAFFIC_PER
 
 from logging import getLogger
 
-logger = getLogger(LOGGER_NAME)
+logger = getLogger(__name__)
 
 
 class Hosts:
@@ -23,23 +23,21 @@ class Hosts:
         Should return the hosts dict
 
         A link should have the following format:
-        {
+        "LINK_NAME"(name of the link in your IRM): {
             "LINK_SPEED": speed in bits,
-            "LINK_NAME": name of the link in your IRM
             "LINK_MAX_TRAFFIC_PERCENTAGE": max traffic percentage for the link
             "LINK_HISTERESYS": histeresys as percentage for the link
         }
         """
-        host_links = hosts["LINKS"]
-        for link in host_links:
-            # checking if link has a max traffic percentage key
-            if "MAX_TRAFFIC_PERCENTAGE" not in host_links:
-                logger.debug("Setting max traffic percentage for %s", link["LINK_NAME"])
-                link["LINK_MAX_TRAFFIC_PERCENTAGE"] = DEFAULT_MAX_TRAFFIC_PERCENTAGE
-
-            # checking if link has a histerysis key
-            if "LINK_HISTERESYS" not in host_links:
-                logger.debug("Setting histeresys for %s", link["LINK_NAME"])
-                link["LINK_HISTERESYS"] = DEFAULT_LINK_HISTERESYS
-
+        logger.info("Formatting links")
+        hosts_names = list(hosts.keys())
+        for host_name in hosts_names:
+            if "LINK_MAX_TRAFFIC_PERCENTAGE" not in hosts[host_name]:
+                logger.debug("Adding default max traffic percentage to %s", host_name)
+                hosts[host_name][
+                    "LINK_MAX_TRAFFIC_PERCENTAGE"
+                ] = DEFAULT_MAX_TRAFFIC_PERCENTAGE
+            if "LINK_HISTERESYS" not in hosts[host_name]:
+                logger.debug("Adding default histeresys to %s", host_name)
+                hosts[host_name]["LINK_HISTERESYS"] = DEFAULT_LINK_HISTERESYS
         return hosts
