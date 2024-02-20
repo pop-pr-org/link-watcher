c4fac1b6a99b3fd3de90767c53d6a05d21f2496e
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-02-20 13:55:35 -0300

    [removed] cron scripts are now a user task

--- cron/daily-watcher.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/docker run --rm --name link-watcher -v /link-watcher/volumes/watcher:/tmp/watcher --env-file /link-watcher/.env link-watcher
--- cron/weekly-alert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/docker run --rm --name link-watcher -v /link-watcher/volumes/watcher:/tmp/watcher --env-file /link-watcher/.env --entrypoint "python3" link-watcher alert.py -d /tmp/watcher/ -f /tmp/watcher/hosts.json
